ae5c489e3517cfd8196517fed687ca976e31bf39
patrick hoehn <patrh_99@hotmail.com>
2019-11-08 21:12:01 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -25,11 +25,13 @@ def find_greater_pair(src)
 end
 
 def total_even_pairs(src)
-  # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
-  # if both numbers in the pair are even, then add both those numbers to the
-  # total
-  #
-  # As a reminder any number % 2 will return 0 or 1. If the result is 0, then
-  # the number was even. Review the operator documentation if you've forgotten
-  # this!
+  row_index = 0 
+  even_pairs_totals = 0
+  while row_index < src.length do
+    if src[row_index][0] %2 = 0 && src[row_index][1] %2 = 0 
+      even_pairs_totals += src[row_index][0] + src[row_index][1]
+    end
+    row_index += 1
+  end
+  return even_pairs_totals
 end
